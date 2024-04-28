717a957b9a9dcaf89d39e7094b81492145bf871f
IT20075330 <89197287+dilakshi423@users.noreply.github.com>
2024-04-28 12:22:06 +0530

    add new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,7 @@ contactMeBtn.onclick =() => {
             setTimeout(() => {
                 page.style.zIndex = 20 + index;
             }, 500)
+
         }, (index + 1) * 200 + 100)
     })
 }
@@ -46,17 +47,51 @@ function reverseIndex() {
         pageNumber = totalpages -1;
     }
 }
-
+// back profile button when click
 const backprofileBtn = document.querySelector('.back-profile');
 
 backprofileBtn.onclick = () => {
     pages.forEach((_, index) => {
         setTimeout(() => {
+            reverseIndex();
             pages[pageNumber].classList.remove('turn');
 
             setTimeout(() => {
-                pages[pageNumber].classList.remove('turn');
-            })
-        }, (index+1) * 200 +100)
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+        }, (index + 1) * 200 +100)
     })
 }
+
+//opening animation
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
+
+//opening animation(cover Rignt animation)
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100) 
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1; 
+}, 2800)
+
+//opening animation(page left or profile page animation)
+setTimeout(() => {
+    pageLeft.style.zIndex = 20; 
+}, 3200)
+
+//opening animation(all pages animation)
+
+pages.forEach((_, index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+        }, 500)
+    }, (index + 1) * 200 +2100)
+})
